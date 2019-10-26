eae7ca9ed0426788e37783e0e0ccae2bc64aca64
Harshal More <moreharshal92@gmail.com>
2019-10-26 03:59:26 -0500

    Update char_occurrence.py

--- char_occurrence.py
+++ char_occurrence.py
@@ -1,17 +1,12 @@
 '''
+Author : Harshal More
 Code created on: 16th Oct, 2019 (Google coding interview question - Phone screen round)
 
-This program returns the starting index and ending index of the 
-consecutive repeated characters if the character is repeated more than n times.
-Here, n is called 'repeat_threshold' and it is provided by the user.
+This program returns the starting index and ending index of the consecutive repeated characters if the character is repeated more than n times. Here, n is called 'repeat_threshold' and it is provided by the user.
 
-for e.g. input 'Heeeello' for repeat_threshold 2 would return e:[1,4] 
-as the letter 'e' is repeated more than twice consecutively. Here 1 is 
-the start index of the repeated character(i.e. 'e' in this case) and 
-4 is the end index. 
+For e.g. input 'Heeeello' for repeat_threshold 2 would return {'e':[(1,4)]} as the letter 'e' is repeated more than twice consecutively. Here 1 is the start index of the repeated character(i.e. 'e' in this case) and 4 is the end index. similarly, input 'Hiiii, what uppp?' for repeat_threshold 2 would return {'i':[(1,4)], 'p':[(13,15)]}.
 
-similarly, input 'Hiiii, what uppp?' for repeat_threshold 2 would 
-return i:[1,4] p:[13,15]
+This program also provides an option to select your own text file as an input. Just make sure that you keep both .py file and .txt file in the same folder.
 '''
 #a = "wowww!fff"
 #a = "Helloooo!eee,oooo ffff"
